ec6e06fea4de191b6102e3b248d18cefd7990dee
ChiaoChuo <kurimulion@gmail.com>
2021-04-08 14:02:49 +0800

    Add testbench for channel_ctrl and refactor channel_ctrl

--- src/channel_ctrl.h
+++ src/channel_ctrl.h
@@ -5,45 +5,15 @@
 typedef ap_uint<4> uint4;
 typedef ap_uint<WIDTH> ctrl_type;
 
-class control_mult {
+ void BLOCK0 (hls::stream<uint4> &din,
+			  hls::stream<uint4> &dout,
+			  hls::stream<ctrl_type> &ctrl_in,
+			  hls::stream<ctrl_type> &ctrl_out);
     
-  hls::stream<uint4> data_int;
-  hls::stream<ctrl_type> ctrl_int;
-    
-  #pragma hls_design
-  void BLOCK0 (hls::stream<uint4> &din,
-               hls::stream<uint4> &dout,
-               hls::stream<ctrl_type> &ctrl_in,
-               hls::stream<ctrl_type> &ctrl_out) {
-#pragma HLS PIPELINE II=1
-    ctrl_type temp_int = ctrl_in.read();
-    ctrl_out.write(temp_int); // one write
-    WRITE:for (ctrl_type i = 0; i != temp_int; i++) {
-      dout.write(din.read() * 13);
-    }
-  }
-    
-  #pragma hls_design
-  void BLOCK1 (hls::stream<uint4> &din,
-               hls::stream<uint4> &dout,
-               hls::
-#pragma HLS PIPELINE II=1
-stream<ctrl_type> &ctrl) {
-    ctrl_type temp_int = ctrl.read(); // one read
-    READ:for (ctrl_type i=0; i != temp_int; i++) {
-      dout.write(din.read());
-    }
-  }
-    
-  public:
-    
-  control_mult () {}
-    
-  #pragma hls_degisn interface
-  void top (hls::stream<uint4> &din,
-            hls::stream<uint4> &dout,
-            hls::stream<ctrl_type> &ctrl) {
-    BLOCK0(din,data_int,ctrl,ctrl_int);
-    BLOCK1(data_int,dout,ctrl_int);
-  }
-};
+void BLOCK1 (hls::stream<uint4> &din,
+		     hls::stream<uint4> &dout,
+		     hls::stream<ctrl_type> &ctrl);
+
+void top (hls::stream<uint4> &din,
+          hls::stream<uint4> &dout,
+          hls::stream<ctrl_type> &ctrl);
---
+++ src/channel_ctrl_tb.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "hls_stream.h"
+#include "channel_ctrl.h"
+
+#define len 4
+
+using namespace std;
+
+int main(void) {
+    hls::stream<uint4> din, dout;
+    hls::stream<ctrl_type> ctrl;
+    uint4 output;
+    int pass = 1;
+    
+    for(int i=0; i<len; i++)
+        din.write(i);
+
+    ctrl.write(len);
+
+    top(din, dout, ctrl);
+
+    cout << ">> Start Checking" << endl;
+    cout << "------------------------" << endl;
+    for(int i=0; i<len; i++){
+    	output = dout.read();
+    	cout << "Output: " << output << "\tExpect: " << uint4(i * 13) << endl;
+        if(output != uint4(i * 13))
+            pass = 0;
+    }
+    cout << "------------------------" << endl;
+    
+    if(!pass) {
+        cout << "Failed" << endl;
+        return 1;
+    }
+
+    ::cout << "Passed Congrats" << endl;
+    return 0;
+}
